5c7c0857ea8684ca78accf501f457bee44e7a8eb
RAFAEL PEREIRA DE OLIVEIRA ARAUJO <24497@fiecalunos.lan>
2019-12-02 22:52:57 -0300

    segundo git

--- app/web/footer.php
+++ app/web/footer.php
@@ -1,106 +1,90 @@
+<div class="fixed-action-btn horizontal">
+    <a class="btn-floating btn-large blue">
+      <i class="large material-icons">mode_edit</i>
+    </a>
+    <ul>
+      <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
+      <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
+      <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
+      <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
+    </ul>
+  </div>
 <footer>
-<div id="engadget-footer-sitemap-container">
-  <footer id="engadget-footer-sitemap">
-    <div class="links">
-      <div class="link-column">
-        <ul class="menu vertical">
-          <li><a href="#"> <strong>Portifolio</strong></a></li>
-          <li><a href="#">Principais Posts</a></li>
-          <li><a href="#">Ultimos Posts</a></li>
-        </ul>
-      </div>
-      <div class="link-column">
-        <ul class="menu vertical">
-          <li><a href="#">Blog</a></li>
-          <li><a href="#"></a></li>
-          <li><a href="#"></a></li>
-          <li><a href="#"></a></li>
-        </ul>
-      </div>
-       <div class="link-column">
-        <ul class="menu vertical">
-          <li><a href="#">Trabalhe Conosco</a></li>
-          <li><a href="#"></a></li>
-          <li><a href="#"></a></li>
-          <li><a href="#"></a></li>
-        </ul>
-      </div>
-      <div class="link-column">
-        <ul class="menu vertical">
-          <li><a href="#"></a></li>
-          <li><a href="#"></a></li>
-          <li><a href="#"></a></li>
-          <li><a href="#"></a></li>
-        </ul>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="js/all.js"></script>
+
+<!------ Include the above in your HEAD tag ---------->
+
+<!-- Social Footer, Colour Matching Icons -->
+<!-- Include Font Awesome Stylesheet in Header -->
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+<!-- // -->
+
+ <!-- Footer -->
+<footer class="page-footer font-small" style="background-color: #e3f2fd;">
+<div class="container">
+<!-- Footer Elements -->
+<div class="container">
+
+  <!-- Grid row-->
+  <div class="row">
+
+    <!-- Grid column -->
+    <div class="col-md-12 py-5">
+      <div class="mb-5 flex-center">
+
+        <!-- Facebook -->
+        <a class="fb-ic">
+          <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+        </a>
+        <!-- Twitter -->
+        <a class="tw-ic">
+          <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+        </a>
+        <!-- Google +-->
+        <a class="gplus-ic">
+          <i class="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+        </a>
+        <!--Linkedin -->
+        <a class="li-ic">
+          <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+        </a>
+        <!--Instagram-->
+        <a class="ins-ic">
+          <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+        </a>
+        <!--Pinterest-->
+        <a class="pin-ic">
+          <i class="fab fa-pinterest fa-lg white-text fa-2x"> </i>
+        </a>
       </div>
     </div>
-  </footer>
-</div>
-<div id="engadget-footer-social-container">
-    <footer id="engadget-footer-social">
-        <div class="footer-left">
-            <div class="newsletter">
-                <h6>Cadastre seu email para receber as novidades</h6>
-                <div class="input-group">
+    <!-- Grid column -->
 
-                    <input class="input-group-field" type="email" placeholder="Endereço de Email">
-                    <div class="input-group-button">
-                        <input type="submit" class="button" value="Inscrever">
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="footer-right">
-                       <div class="rounded-social-buttons">
-            <h6>Siga nos</h6>
-  <a class="social-button facebook" href="#"></a>
-  <a class="social-button twitter" href="#"></a>
-  <a class="social-button linkedin" href="#"></a>
-  <a class="social-button youtube" href="#"></a>
-  <a class="social-button instagram" href="#"></a>
+  </div>
+  <!-- Grid row-->
+
 </div>
-        </div>
-    </footer>
+<!-- Footer Elements -->
+
+<!-- Copyright -->
+<div class="footer-copyright text-center py-3">© 2020 Copyright:
+  <a href="https://mdbootstrap.com/education/bootstrap/"> HomeTECH.com</a>
 </div>
-<div id="engadget-footer-contact-details-container">
-    <footer id="engadget-footer-contact-details">
-        <div class="footer-left">
-            <div class="contact-details">
-                <ul>
-                    <li><img class="thumbnail" src="http://placehold.it/80"></li>
-                    <li><i class="fa fa-phone fa-lg" aria-hidden="true"></i> +55 19 981858526</li>
-                    <li><a data-toggle="animatedModal10"><i class="fa fa-envelope-o" aria-hidden="true"></i> Contact us</a></li>
-                    <li><i class="fa fa-map-marker fa-lg" aria-hidden="true"></i> Rua Augusto , City, County, Country</li>
-                </ul>
-            </div>
-        </div>
-        <div class="footer-right">
-            <ul class="menu align-right">
-                <li><a href="#">One</a></li>
-                <li><a href="#">Two</a></li>
-                <li><a href="#">Three</a></li>
-                <li><a href="#">Four</a></li>
-            </ul>
-        </div>
-    </footer>
-</div>
+<!-- Copyright -->
+
+</footer>
+<!-- Footer -->
 
 </footer>
 
 
 
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>   
-<script src="js/vendor/jquery.js"></script>
-    <script src="js/vendor/what-input.js"></script>
-    <script src="js/vendor/foundation.min.js"></script>
-    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-<script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
-    <script>
-      $(document).foundation();
-    </script>
+
 </body>
 </html>
