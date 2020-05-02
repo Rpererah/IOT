cbacd1d4839552162ed7c77f0fb90948212550f5
Pedro <pedrocosta2003@gmail.com>
2020-05-02 13:27:41 -0300

    manutencao do footer

--- app/web/footer.php
+++ app/web/footer.php
@@ -5,7 +5,7 @@
         <div class="col-lg-8 col-md-10 mx-auto">
           <ul class="list-inline text-center">
             <li class="list-inline-item">
-              <a href="https://twitter.com">
+              <a target="_blank" href="https://twitter.com">
                 <span class="fa-stack fa-lg">
                   <i class="fas fa-circle fa-stack-2x"></i>
                   <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
@@ -13,7 +13,7 @@
               </a>
             </li>
             <li class="list-inline-item">
-              <a href="https://facebook.com">
+              <a target="_blank" href="https://facebook.com">
                 <span class="fa-stack fa-lg">
                   <i class="fas fa-circle fa-stack-2x"></i>
                   <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
@@ -21,7 +21,7 @@
               </a>
             </li>
             <li class="list-inline-item">
-              <a href="https://github.com/AutoGuResRaPeJu/IOT">
+              <a target="_blank" href="https://github.com/AutoGuResRaPeJu/IOT">
                 <span class="fa-stack fa-lg">
                   <i class="fas fa-circle fa-stack-2x"></i>
                   <i class="fab fa-github fa-stack-1x fa-inverse"></i>
@@ -29,7 +29,7 @@
               </a>
             </li>
           </ul>
-          <p class="copyright text-muted">Copyright &copy; HomeTech.com 2019</p>
+          <p class="copyright text-muted">Copyright &copy; HomeTech <?php echo date("Y"); ?> </p>
         </div>
       </div>
     </div>
@@ -44,6 +44,10 @@
   <!-- Bootstrap core JavaScript -->
   <script src="vendor/jquery/jquery.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Contact Form JavaScript -->
+  <script src="js/jqBootstrapValidation.js"></script>
+  <script src="js/contact_me.js"></script>
 
   <!-- Custom scripts for this template -->
   <script src="js/clean-blog.min.js"></script>
--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
-
+//lembre de fazer o git 30/04/2020
 session_start(); 
-if(isset($_SESSION['usuario']))
+if(!isset($_SESSION['usuario']))
 {
     header ('Location: app/web/Entrada.php');
+} else {
+    header ('Location: app/web/index.php');
 }
 
 ?>
